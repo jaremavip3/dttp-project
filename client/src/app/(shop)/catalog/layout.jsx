c9a52edb6637dc3501257b56788cf08c85bed12c
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-06-06 14:49:35 -0400

    Add CatalogLayout component with filter and flexible children rendering

--- client/src/app/(shop)/catalog/layout.jsx
+++ client/src/app/(shop)/catalog/layout.jsx
@@ -1,1 +1,10 @@
-//This page will have compoenents that are common to all dynamic routes and catalog layout
+import Filter from "@/components/Filter";
+
+export default function CatalogLayout({ children }) {
+  return (
+    <div className="flex">
+      <Filter />
+      <div className="flex-1">{children}</div>
+    </div>
+  );
+}
